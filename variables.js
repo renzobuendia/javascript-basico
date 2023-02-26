6b5b0ff39e7a86020f7c87d5791fef888f0da621
renzobuendia <renzobuendia@gmail.com>
2023-02-25 22:33:35 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Renzo"
+
+//declarar
+var edad;
+
+edad
+//inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Renzo",
+    edad: 30
+}
+
+persona;
