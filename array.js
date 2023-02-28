8809eb1a91084c7f97f6cb13821d8f37ca9932a1
Renzo Buendia Quinones <renzobuendia@gmail.com>
2023-02-28 14:17:48 -0500

    loop for and for of

--- array.js
+++ array.js
@@ -1,1 +1,11 @@
 var frutas = ["Manzana", "Platano", "Cereza", "Fresa"];
+
+console.log(frutas);
+
+// 
+frutas.pop() //elimina el ultimo elemento
+frutas.push("Uva") //agrega un elemento al final
+frutas.unshift("Uva")//agrega un elemento al inicio
+frutas.shift() //Elimina un elemento del inicio
+
+frutas.indexOf("Platano")//Busca un elemento en el array
---
+++ for.js
@@ -0,0 +1,10 @@
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"]
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola, ${estudiante}`);
+}
+
+// for(var i = 0; i < estudiantes.length; i++){
+//     saludarEstudiantes(estudiantes[i]);
+// }
+for(var estudiante of estudiantes)
