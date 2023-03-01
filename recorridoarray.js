679a59bc7ceda86d74ff9bccce18c788cc99ebe3
Renzo Buendia Quinones <renzobuendia@gmail.com>
2023-03-01 13:53:07 -0500

    find, for each and some in arrays2

--- recorridoarray.js
+++ recorridoarray.js
@@ -42,5 +42,36 @@ var articulos = [
 ]
 
 var encuentraArticulo = articulos.find(function(articulo){
-    return articulo.nombre === "Laptop"
+    return articulo.nombre === "Laptop" && articulo.costo == 25000
+})
+
+//forEach
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre: "Television", costo: 2500},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Laptop", costo: 20000},
+    {nombre: "Laptop", costo: 25000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700}
+]
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+})
+
+//some
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre: "Television", costo: 2500},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Laptop", costo: 20000},
+    {nombre: "Laptop", costo: 25000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700}
+]
+
+var articulosBaratos = articulos.some(function(articulo){
+    return articulo.costo <= 700
 })
